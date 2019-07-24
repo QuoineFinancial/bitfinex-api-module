28d734a3c06192696d401fc8181d1ea4ff90635f
Max Levy <35595512+maxim-levy@users.noreply.github.com>
2019-07-24 10:11:50 +0900

    install doesn't require much

--- setup.py
+++ setup.py
@@ -6,16 +6,6 @@ setup(name='bitfinex-api-module',
       version='0.2.2',
       description='Bitfinex API client.',
       url='https://github.com/QuoineFinancial/bitfinex-api-module',
-      install_requires=[
-          'eventemitter==0.2.0',
-          'asyncio==3.4.3',
-          'websockets==7.0',
-          'pylint==2.2.2',
-          'pytest-asyncio==0.6.0',
-          'six==1.12.0',
-          'pyee==5.0.0',
-          'aiohttp==3.4.4',
-      ],
       packages=['bfxapi', 'bfxapi.models', 'bfxapi.websockets',
                 'bfxapi.rest',  'bfxapi.utils', ]
       )
